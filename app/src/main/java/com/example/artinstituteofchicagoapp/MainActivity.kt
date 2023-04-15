02c538bc964965d93e668fcebf92b8f84be1e511
amadeusjustinn <amadeus.haryanto@codagami.com>
2023-04-14 19:58:45 -0500

    Resolve merge conflicts

---
+++ app/src/main/java/com/example/artinstituteofchicagoapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.artinstituteofchicagoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnShowArtworks = findViewById<Button>(R.id.showArtworks)
+        btnShowArtworks.setOnClickListener {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/artinstituteofchicagoapp/functions/ButtonClick.kt
@@ -0,0 +1,7 @@
+package com.example.artinstituteofchicagoapp.functions
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ButtonClick : AppCompatActivity() {
+//    override fun onClick
+}
