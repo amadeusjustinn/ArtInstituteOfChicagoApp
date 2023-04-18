34a525152e6d3f3c2c83c95d0c192683e7397b4e
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2023-04-17 23:18:23 -0500

    Minor adjustments

--- app/src/main/java/com/example/artinstituteofchicagoapp/MainActivity.kt
+++ app/src/main/java/com/example/artinstituteofchicagoapp/MainActivity.kt
@@ -6,14 +6,14 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-        val btnShowArtworks = findViewById<Button>(R.id.showArtworks)
-        btnShowArtworks.setOnClickListener {
-            val intent = Intent(this, GalleryView::class.java)
-            startActivity(intent)
-        }
-    }
+		val btnShowArtworks = findViewById<Button>(R.id.showArtworks)
+		btnShowArtworks.setOnClickListener {
+			val intent = Intent(this, GalleryView::class.java)
+			startActivity(intent)
+		}
+	}
 }
